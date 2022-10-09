f60c2139638c25bd2118a0ddbe02a15a892b0f3d
MaxMax1313 <115236599+MaxMax1313@users.noreply.github.com>
2022-10-09 16:53:19 +0300

    Add files via upload

---
+++ TretiyClass.java
@@ -0,0 +1,12 @@
+package Lesson7_1;
+
+import Lesson7.Employee;
+
+public class TretiyClass {
+    public static void main(String[] args) {
+        Employee emp = new Employee(500);
+        System.out.println(emp.salary);
+        emp.dvoynayaZP();
+
+    }
+}
